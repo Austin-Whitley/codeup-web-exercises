1c06c75fb8c29545efc59989a3889a7697533449
Austin Whitley <whitley.austin01@gmail.com>
2021-03-18 15:28:31 -0500

    completed the JS with HTML lecture

---
+++ js/external-lec.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var message = "Howdy y'all";
+var whatIsThisThen = message + " hope you're having a great day.";
+
+console.log(whatIsThisThen);
+
+var favoriteCheese = prompt("What is your favorite type of cheese?");
+console.log(favoriteCheese + " is the cheese of choice!");
+
+var typeOfPrompt = typeof prompt("tell me the thing.");
+console.log(typeOfPrompt);
+
+var num = parseFloat(prompt("gimme a number: "));
+console.log("User entered: " + num + ". That number plus 17 is: " + (num + 17) + ".");
+
+//returns a boolean value
+var canIGoToTheStore = confirm("Can I go to the store, pleease?");
+console.log(canIGoToTheStore);
+
+// returns an undefined variable
+alert("You're looking at a website!");
