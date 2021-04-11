e271ab2920fc29a2b3187f8c5124cd7b0d1032b3
Austin Whitley <whitley.austin01@gmail.com>
2021-04-10 22:47:42 -0500

    Questions for the morning

---
+++ js/messages.js
@@ -0,0 +1,13 @@
+/**
+ * Search how Javascript can interact with HTML
+ * 1. How do I call upon a specific <div> to appear
+ *    on screen to the user?
+ *
+ * Ex: Show the error message when someone clicks a button on screen
+ *
+ * 2. How do I make that message appear, then disappear shortly after?
+ *
+ * 3. Is it possible to make that error message appear in a specific
+ *    location on the page?
+ **/
+
