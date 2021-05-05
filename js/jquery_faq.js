2026233aa89015a2aedb2d39dd882ffcdf5c2e99
Austin Whitley <whitley.austin01@gmail.com>
2021-05-05 10:52:57 -0500

    minor changes added to jQuery faq pages

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -8,11 +8,32 @@ $("dt").click(function(){
 $("#last-list-element").click(function(){
     $("ul").children().next().next().next().css("background-color", "yellow");
 });
+
 //bold the li elements when clicking on the h3 title for that list
 $("h3").click(function(){
     $(this).next().css("font-weight", "bold");
 })
 
+//color the first element blue if you click inside the list
 $("ul").click(function(){
     $(this).children().first().css("color", "blue");
 });
+
+$("dt").click(function(){
+    $(this).toggleClass("highlight");
+})
+
+$("#toggleInvis").click(function(){
+    $("dd").removeClass("invisible");
+})
+
+
+
+//
+// $('#last-list-element').click(function(){
+//     $('li').each(function(index){
+//         if(index === 11 || index === 15 || index === 19){
+//             $(this).css("color", "red");
+//         }
+//     });
+// });
