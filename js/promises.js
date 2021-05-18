7b145773db98399235411f2548ed515fb797e9ea
Austin Whitley <whitley.austin01@gmail.com>
2021-05-18 10:08:08 -0500

    updated the promises file to include a working version of the wait function

--- js/promises.js
+++ js/promises.js
@@ -5,25 +5,28 @@ function getGithubUsernames(userName) {
     return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': GITHUB_ACCESS_TOKEN}})
         .then(response => response.json().then(data => {
             console.log(data);
-            var latestPush = data[0].created_at
-            let date = new Date(latestPush);
+            for(var i = 0; i < data.length; i++) {
+                if(data[i].type === "PushEvent") {
+                    var latestPush = data[i].created_at
+                    let date = new Date(latestPush);
+                    console.log(date.toString());
+                    break;
+                }
+            }
 
-            console.log(date.toString());
         }))
 }
 getGithubUsernames("Austin-Whitley")
 
 
 
-function wait(number) {
-    return new Promise((resolve, reject) => {
+const wait = waitTime => {
+    return new Promise(resolve => {
         setTimeout(() => {
-            if (Math.random() > 0.1) {
-                resolve('Here is your data: ...');
-            } else {
-                reject('Network Connection Error!');
-            }
-        }, number);
-    });
+            resolve();
+        }, waitTime)
+    })
 }
-wait(1500)
+
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
