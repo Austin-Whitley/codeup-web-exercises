e7fb62fe3fb04bba241d6afeb2a2345e2482cd63
Austin Whitley <whitley.austin01@gmail.com>
2021-05-18 09:42:12 -0500

    moved the script into its own file and also deleted the raw access token

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,29 @@
 "use strict";
-fetch(url, {headers: {'Authorization': 'token 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'}}).then()
+"use strict";
+
+function getGithubUsernames(userName) {
+    return fetch(`https://api.github.com/users/${userName}/events`, {headers: {'Authorization': GITHUB_ACCESS_TOKEN}})
+        .then(response => response.json().then(data => {
+            console.log(data);
+            var latestPush = data[0].created_at
+            let date = new Date(latestPush);
+
+            console.log(date.toString());
+        }))
+}
+getGithubUsernames("Austin-Whitley")
+
+
+
+function wait(number) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.1) {
+                resolve('Here is your data: ...');
+            } else {
+                reject('Network Connection Error!');
+            }
+        }, number);
+    });
+}
+wait(1500)
